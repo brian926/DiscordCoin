9b244c16d54742921ef2f87d1f13a369ed07e3f8
Brian Antunes <brian.antunes@yahoo.com>
2023-07-31 16:54:14 -0400

    Added to shop.js

--- commands/shop.js
+++ commands/shop.js
@@ -1,6 +1,6 @@
 const { SlashCommandBuilder } = require("discord.js");
 const profileModel = require("../models/profileSchema");
-const { customRoleCost } = require("../shopPrices.json");
+const { customRoleCost, custonRoleEdit } = require("../shopPrices.json");
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -9,7 +9,23 @@ module.exports = {
         .addSubcommand((subcommand) =>
             subcommand
                 .setName("custom-role")
-                .setDescription(`Buy a custom role for ${customRoleCost} coins`)
+                .setDescription(`Buy or edit a custom role`)
+                .addStringOption((option) =>
+                    option
+                        .setName("action")
+                        .setDescription("Choose to edit or buy a custom role")
+                        .addChoices(
+                            {
+                                name: `Buy a Role for ${customRoleCost} coins`,
+                                value: "buy"
+                            },
+                            {
+                                name: `Edit Role for ${custonRoleEdit} coins`,
+                                value: "edit"
+                            }
+                        )
+                        .setRequired(true)
+                )
                 .addStringOption((option) =>
                     option
                         .setName("name")
@@ -24,48 +40,110 @@ module.exports = {
                         .setDescription("Choose a color for your role")
                         .addChoices(
                             { name: "Red", value: "Red" },
-                            { name: "Cyan", value: "Cyan" },
-                            { name: "Blue", value: "Blue" },
+                            { name: "Greyple", value: "Greyple" },
+                            { name: "Blurple", value: "Blurple" },
                             { name: "Yellow", value: "Yellow" },
-                            { name: "Magenta", value: "Magenta" }
+                            { name: "Green", value: "Green" }
                         )
                         .setRequired(true)
                 )
+        )
+        .addSubcommand((subcommand) =>
+            subcommand
+                .setName("custom-role-removal")
+                .setDescription("Delete your custom role")
         ),
     async execute(interaction, profileData) {
-        const { balance, userId } = profileData;
+        const { balance, userId, customRoleId } = profileData;
         const shopCommand = interaction.options.getSubcommand();
 
         if (shopCommand === "custom-role") {
+            const action = interaction.options.getString("action");
             const name = interaction.options.getString("name");
             const color = interaction.options.getString("color");
 
-            if (balance < customRoleCost) {
-                return await interaction.reply({ ephemeral: true, content: `You need ${customRoleCost} coins to buy a custom role` })
+            if (action === "buy") {
+                if (customRoleId !== "") {
+                    return await interaction.reply({ ephemeral: true, content: `You already have a custom role!` })
+                }
+                if (balance < customRoleCost) {
+                    return await interaction.reply({ ephemeral: true, content: `You need ${customRoleCost} coins to buy a custom role` })
+                }
+
+                await interaction.deferReply();
+
+                const customRole = await interaction.guild.roles.create({
+                    name,
+                    permissions: [],
+                    color,
+                });
+
+                interaction.member.roles.add(customRole);
+
+                await profileModel.findOneAndUpdate(
+                    {
+                        userId,
+                    },
+                    {
+                        $set: {
+                            customRoleId: customRole.id,
+                        },
+                        $inc: {
+                            balance: -customRoleCost,
+                        },
+                    }
+                );
+                await interaction.editReply("Successfully purchased a custom role!");
+            }
+            else if (action === "edit") {
+                if (customRoleId === "") {
+                    return await interaction.reply({ ephemeral: true, content: `You need to have a custom role!` })
+                }
+                if (balance < customRoleCost) {
+                    return await interaction.reply({ ephemeral: true, content: `You need ${customRoleCost} coins to buy a custom role` })
+                }
+
+                await interaction.deferReply();
+
+                const editRole = await interaction.guild.roles.fetch(customRoleId);
+
+                editRole.edit({ name, color });
+
+                await profileModel.findOneAndUpdate(
+                    {
+                        userId,
+                    },
+                    {
+                        $inc: {
+                            balance: -custonRoleEdit,
+                        },
+                    }
+                );
+                await interaction.editReply("Successfully edit your custom role!");
+            }
+        } else if (shopCommand === "custom-role-removal") {
+            if (customRoleId === "") {
+                return await interaction.reply({ ephemeral: true, content: `You need to have a custom role!` })
             }
 
-            await interaction.deferReply();
+            await interaction.deferReply({ ephemeral: true });
 
-            const customRole = await interaction.guild.roles.create({
-                name,
-                permissions: [],
-                color,
-            });
+            const customRole = await interaction.guild.roles.fetch(customRoleId);
 
-            interaction.member.roles.add(customRole);
+            customRole.delete();
 
             await profileModel.findOneAndUpdate(
                 {
                     userId,
                 },
                 {
-                    $inc: {
-                        balance: -customRoleCost,
+                    $set: {
+                        customRoleId: "",
                     },
                 }
             );
 
-            await interaction.editReply("Successfully purchased a custom role!");
+            await interaction.editReply("Successfully removed your custom role!");
         }
     }
 };
--- models/profileSchema.js
+++ models/profileSchema.js
@@ -8,6 +8,7 @@ const profileSchema = new mongoose.Schema({
     weeklyLastUsed: { type: Number, default: 0 },
     monthlyLastUsed: { type: Number, default: 0 },
     coinflipLastUsed: { type: Number, default: 0 },
+    customRoleId: { type: String, default: "" },
 });
 
 const model = mongoose.model("dinodb", profileSchema);
